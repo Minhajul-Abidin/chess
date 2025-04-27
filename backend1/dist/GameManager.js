82e7caa8284733023ce6e0c21d785631c9f657f5
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2025-04-27 14:55:44 +0530

    first commit: project launch 🚀

---
+++ backend1/dist/GameManager.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameManager = void 0;
+class GameManager {
+    constructor() {
+        this.games = [];
+    }
+    addUser(socket) { }
+    removeUser(socket) { }
+    handleMessage() { }
+}
+exports.GameManager = GameManager;
